a00b968989a35b33568ad36b348f4ef1f510fcc2
NintendoZaedus <42098470+NintendoZaedus@users.noreply.github.com>
2020-05-25 02:22:37 -0500

    Access from more than just the root dir

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 const fs = require('fs');
 const yargs = require('yargs')
+const path = require('path');
 
 const empty = () => {};
-const call = (argv) => (require(`./commands/${argv._[0]}`)).run(argv);
+const call = (argv) => (require(path.join(__dirname, `/commands/${argv._[0]}`))).run(argv);
 
-const files = fs.readdirSync('./commands/').map(c => c.substring(0, c.length - 3));
+const files = fs.readdirSync(path.join(__dirname, '/commands/')).map(c => c.substring(0, c.length - 3));
 
 for(file of files) {
-    const cmd = require(`./commands/${file}`);
+    const cmd = require(path.join(__dirname, `/commands/${file}`));
     yargs.command(cmd.name, cmd.description, cmd.args ? cmd.args : empty, call)
 }
 
